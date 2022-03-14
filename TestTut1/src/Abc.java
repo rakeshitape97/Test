83fbd318041f69aee323e54e75cdb12ed471486a
Rakesh Itape <rakeshitape844@gmail.com>
2022-03-14 16:21:28 +0530

    Now add the 2 sentences

--- TestTut1/src/Abc.java
+++ TestTut1/src/Abc.java
@@ -4,7 +4,9 @@ public class Abc
 	public static void main(String[] args) 
 	{
 	
-		System.out.println("Start the code....");
+		System.out.println("Start the program...");
+		System.out.println("The Code is running fine...");
+		System.out.println("End the program...");
 	}
 
 }
