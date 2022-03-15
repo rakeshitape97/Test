5fa67b42738c0fb52b45c4701d9936dcafc4d51d
Rakesh Itape <rakeshitape844@gmail.com>
2022-03-15 11:26:33 +0530

    add hungry and thirsty

---
+++ TestTut2/src/com/TechM/Tut2.java
@@ -0,0 +1,24 @@
+package com.TechM;
+
+public class Tut2 
+{
+	static boolean a = true;
+	static boolean b = false;
+	
+	public static void main(String[] args) 
+	{
+	
+		
+		if(a)
+		{
+			System.out.println("I am hungry...");
+		}
+		else
+		{
+			System.out.println("I am thirsty...");
+		}
+		
+		
+	}
+
+}
