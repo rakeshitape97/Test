bf83a70c21b9905da1e0f73ea1bc0c38ec91eb2d
Rakesh Itape <rakeshitape844@gmail.com>
2022-03-15 11:45:36 +0530

    hungry and thirsty commit

--- TestTut1/src/Abc.java
+++ TestTut1/src/Abc.java
@@ -1,12 +1,25 @@
 
 public class Abc 
 {
-	public static void main(String[] args) 
-	{
-	
-		System.out.println("Start the program...");
-		System.out.println("The Code is running fine...");
-		System.out.println("End the program...");
-	}
+	static boolean a = true;
+	static boolean b = false;
+
+//	
+//		System.out.println("Start the program...");
+//		System.out.println("The Code is running fine...");
+//		System.out.println("End the program...");
+			
+		public static void main(String[] args) 
+		{
+		
+			if(a)
+			{
+				System.out.println("I am hungry...");
+			}
+			else
+			{
+				System.out.println("I am thirsty...");
+			}
+		}
 
 }
